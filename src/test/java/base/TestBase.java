94052007ae0dde3d08fa0ca696e473f2b3270cc8
7_bondarevana <tasha2k7@mail,ru>
2025-04-05 13:06:47 +0300

    new structure TestSelenide project

---
+++ src/test/java/base/TestBase.java
@@ -0,0 +1,36 @@
+package base;
+
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static helpers.EnvironmentData.getEnvironmentData;
+
+public class TestBase {
+
+    @BeforeMethod
+    public void methodSetup() {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        //caps.setVersion();
+        caps.setPlatform(Platform.valueOf(System.getProperty("platform", "MAC")));
+        caps.setAcceptInsecureCerts(true);
+        Configuration.browser = System.getProperty("browser", "safari");
+        Configuration.remote = " http://169.254.170.176:4444/wd/hub";
+        Configuration.browserCapabilities = caps;
+        Configuration.pageLoadTimeout = 10000;
+        Configuration.timeout = 10000;
+
+        open(getEnvironmentData("Dev.url"));
+        getWebDriver().manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void methodTeardown() {
+        getWebDriver().quit();
+    }
+}
